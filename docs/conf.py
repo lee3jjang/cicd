7df44d0d97e032b104521bab915c59ab15857c0c
Sang-Jin <lee3jjang@gmail.com>
2021-07-17 20:08:23 +0900

    dfdf

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ author = 'lee3jjang'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
